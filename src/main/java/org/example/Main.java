aad40760616f9d61c8401754b7e2db5e03040c26
MarsiPeto <marcelina.peto09@gmail.com>
2024-09-01 13:25:49 +0100

    sessionfactory main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,14 @@
 package org.example;
 
+import org.example.employee.EmployeeEntity;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        final SessionFactory sessionFactory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(EmployeeEntity.class)
+                .buildSessionFactory();
     }
 }
